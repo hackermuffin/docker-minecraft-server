a74edcf9d67451dda3e4b8b32373c029d40772e6
hackermuffin <peteroslington1@gmail.com>
2021-04-16 12:29:40 +1000

    Added support for running servers off provided scripts

--- server-files/entrypoint.sh
+++ server-files/entrypoint.sh
@@ -2,10 +2,12 @@
 
 # Set required variables
 eula=${eula-false}          # Default eula to false
+runscript=false             # Run using script provided by modpack (true/false)
 minram=${minram-1024M}      # Default minimum ram
 maxram=${maxram-1024M}      # Default maximum ram
 name="minecraft-server"     # Set screen session name
 jar="server.jar"            # Set jar to execute
+scriptname=""               # Script name to use if needed
 
 # Use environment variable to keep eula requirement
 if [ $eula != true ]; then
@@ -27,10 +29,17 @@ else
 fi
 
 # Construct command
-CMD="java -Xmx${minram} -Xms${maxram} ${javaargs} -jar ${jar} nogui"
+if [ $runscript == false ]; then
+  CMD="java -Xmx${minram} -Xms${maxram} ${javaargs} -jar ${jar} nogui"
+elif [ $runscript == true ]; then
+  CMD="./${scriptname}"
+else
+  echo "Please use a boolean value for runscript"
+  exit
+fi
 
 # Output the command to the logs.
 echo "Attempting to use a screen session called ${name} to run: ${CMD}"
 
-# Run the command in a screen terminal, redirecting incorrect error message to /dev/null
-screen -S minecraft-server sh -c "while :; do ${CMD}; echo Server down. Press Ctrl+C to stop reboot; sleep 10; done" 2> /dev/null
+# Run the command in a screen terminal
+screen -S minecraft-server sh -c "while :; do ${CMD}; echo Server down. Press Ctrl+C to stop reboot; sleep 10; done"
