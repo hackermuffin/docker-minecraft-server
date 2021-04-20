5d00d2027238f8a9744d9083f9537743d21e1505
hackermuffin <peteroslington1@gmail.com>
2021-04-20 21:19:42 +1000

    Moved awk command into shell script

--- server-files/install.sh
+++ server-files/install.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 # Install required programs
-apk add --no-cache screen                   # Screen for console access
+apk add --no-cache screen                       # Screen for console access
 
 # Construct server.properties edit script
-cat > properties.sh << 'EOF'                # Write header to file
+cat > properties.sh << 'EOF_properties_header'  # Write header to file
 #!/bin/sh
 set_server_properties() { 
     property_name=$1
     property_value=$2
     sed -i "s/${property_name}=.*/${property_name}=${property_value}/g" server.properties
 }
-EOF
-awk -v q="'" -F'[-=.]' -f properties.awk server.properties >> properties.sh # Run awk
-chmod +x properties.sh                      # Make script executable
+EOF_properties_header
+cat > properties.awk << 'EOF_properties_awk'    # Write awk command to file
+!/#/ {
+    {printf "set_server_properties "}
+    {printf $1}{for(i=2;i<NF;i++){printf "-%s", $i}}
+    {printf " ${"$1}{for(i=2;i<NF;i++){printf "_%s", $i}}
+    if($NF) {printf "-"q $NF q}{printf "}\n"}
+}
+EOF_properties_awk
+awk -v q="'" -F'[-=.]' -f properties.awk server.properties >> properties.sh
+chmod +x properties.sh                          # Make script executable
